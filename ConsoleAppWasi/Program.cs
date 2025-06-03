953550b01cf7bacf17db05a1cbf41154dcfd0e64
Niels Tanis <nielstanis@live.nl>
2025-06-03 07:57:44 +0200

    global.json SDK + fix

--- ConsoleAppWasi/Program.cs
+++ ConsoleAppWasi/Program.cs
@@ -1,7 +1,4 @@
 using System;
 using System.Runtime.InteropServices;
 
-Console.WriteLine($"Hello Swetugg '24 Göteborg from {RuntimeInformation.OSArchitecture}!");
-
-var text = System.IO.File.ReadAllText("/etc/hosts");
-Console.WriteLine(text);
+Console.WriteLine($"Hello SecAppDev'25 from {RuntimeInformation.OSArchitecture}!");
